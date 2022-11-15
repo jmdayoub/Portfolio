ba76b819cc31fcf37229af773ddf99a9c8052967
jmdayoub <jdayoub@gmail.com>
2022-11-14 16:41:12 -0800

    Initial React App

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-front">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
+}
+
+export default App;
