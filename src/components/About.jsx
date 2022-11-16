ec1535fa2c7d2264453f62ed4b0bd58c71cb1528
jmdayoub <jdayoub@gmail.com>
2022-11-15 18:15:56 -0800

    "Edited about"

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,6 @@ const About = () => {
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Jared.
             <br className="hidden lg:inline-block" />
-            {/* I build web applications! */}
             <Typed
               strings={["I build web applications."]}
               typeSpeed={80}
